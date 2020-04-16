d9d3e0baef8db1f50b13a5391ecaee831ccf9b63
Adil Doshayev <doshayev@gmail.com>
2020-04-15 20:32:19 -0500

    update

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,7 +10,9 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         dryRun = false,
         strict = false,
-        tags = "@dashboard and @driver"
+        //tags = "@dashboard and @driver",
+        tags = "",
+        plugin = "html:target/cucumber-html-report"
 )
 public class CucumberRunner {
 
--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -1,7 +1,22 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
 public class Hooks {
 
+    @Before
+    public void setup(){
+        System.out.println("Test setup");
+    }
+
+
+    @After
+    public void tearDown(){
+        System.out.println("Test clean up");
+        Driver.closeDriver();
+    }
 
 
 }
