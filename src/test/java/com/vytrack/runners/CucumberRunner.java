321c49853191efe3a4fe6a05196bc54d4196766d
Adil Doshayev <doshayev@gmail.com>
2020-04-18 16:20:03 -0500

    updates

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,11 +10,10 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         dryRun = false,
         strict = false,
-        tags = "@activities",
+        tags = "",
         plugin = {
-                "html:target/cucumber-html-report",
+                "html:target/default-report",
                 "json:target/cucumber.json"
-
         }
 
 )
--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -10,27 +10,29 @@ import org.openqa.selenium.TakesScreenshot;
 public class Hooks {
 
     @Before(order = 2)
-    public void setup(){
+    public void setup() {
         System.out.println("Test setup");
         Driver.getDriver().manage().window().maximize();
     }
 
     @Before(value = "@driver", order = 1)
-    public void specialSetup(){
+    public void specialSetup() {
         System.out.println("Setup for driver only");
     }
 
     @After("@driver")
-    public void specialTearDown(){
+    public void specialTearDown() {
         System.out.println("Tear down for driver only");
     }
 
     @After
-    public void tearDown(Scenario scenario){
-        if (scenario.isFailed()){
+    public void tearDown(Scenario scenario) {
+        //how to check if scenario failed
+        if (scenario.isFailed()) {
             TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
             byte[] image = takesScreenshot.getScreenshotAs(OutputType.BYTES);
-            scenario.embed(image,"image/png",scenario.getName());
+            //attach screenshot to the report
+            scenario.embed(image, "image/png", scenario.getName());
         }
         System.out.println("Test clean up");
         Driver.closeDriver();
