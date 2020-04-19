ac559b607fec29eb526077a96fdbd6b0af9cf4de
Adil Doshayev <doshayev@gmail.com>
2020-04-19 18:55:08 -0500

    updates

--- src/test/java/com/vytrack/step_definitions/CreateCarStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/CreateCarStepDefinitions.java
@@ -2,6 +2,7 @@ package com.vytrack.step_definitions;
 
 import com.vytrack.pages.fleet.VehiclesPage;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
 import java.util.List;
 import java.util.Map;
@@ -23,10 +24,28 @@ public class CreateCarStepDefinitions {
         System.out.println(dataTable);
         for (Map<String, String> row : dataTable) {
             vehiclesPage.setLicencePlateInput(row.get("License Plate"));
+
             vehiclesPage.setDriverInput(row.get("Driver"));
+
             vehiclesPage.setLocationInput(row.get("Location"));
+
             vehiclesPage.setModelYear(row.get("Model Year"));
+
             vehiclesPage.setColor(row.get("Color"));
+            //click to save ne car
+            vehiclesPage.clickOnSaveAndClose();
         }
     }
+
+    @Then("user verifies that car info is displayed:")
+    public void user_verifies_that_car_info_is_displayed(List<Map<String, String>> dataTable) {
+        for(Map<String, String> row: dataTable){
+            Assert.assertEquals(row.get("License Plate"), vehiclesPage.getCarGeneralInfo("License Plate"));
+            Assert.assertEquals(row.get("Driver"), vehiclesPage.getCarGeneralInfo("Driver"));
+            Assert.assertEquals(row.get("Location"), vehiclesPage.getCarGeneralInfo("Location"));
+            Assert.assertEquals(row.get("Model Year"), vehiclesPage.getCarGeneralInfo("Model Year"));
+            Assert.assertEquals(row.get("Color"), vehiclesPage.getCarGeneralInfo("Color"));
+        }
+    }
+
 }
